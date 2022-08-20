43df4dac580848a2bfdddbadecf4d7d447f07542
tanaka <lemo.nade.engineer.room@gmail.com>
2022-08-20 13:25:01 +0900

    environment

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,6 +2,8 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+
+const host: string = import.meta.env.VITE_TITLE
 </script>
 
 <template>
@@ -13,7 +15,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Hello Vue + Deno!!" />
+  <HelloWorld :msg="host" />
 </template>
 
 <style scoped>
